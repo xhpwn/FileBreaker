8aebbd2524479e998aaee4a2d18eb50f79cc5e94
xhpwn <saxena20@purdue.edu>
2016-06-05 08:53:30 +0530

    Nimbus Launcher

---
+++ src/filedigger/FileDigger.java
@@ -0,0 +1,14 @@
+package filedigger;
+
+/**
+ *
+ * @author Anubhav Saxena
+ * anubhav@saxena.xyz
+ * http://saxena.xyz
+ */
+public class FileDigger {
+
+    public static void main(String[] args) {
+    }
+    
+}
